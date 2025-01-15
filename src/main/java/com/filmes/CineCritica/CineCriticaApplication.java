e7ce063a1573dbe48b79b7c57290d23b23744e86
Kauê Santos <kaues0577@gmail.com>
2025-01-14 22:57:30 -0300

    Initial commit

---
+++ src/main/java/com/filmes/CineCritica/CineCriticaApplication.java
@@ -0,0 +1,13 @@
+package com.filmes.CineCritica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineCriticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineCriticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filmes/CineCritica/CineCriticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmes.CineCritica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineCriticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
